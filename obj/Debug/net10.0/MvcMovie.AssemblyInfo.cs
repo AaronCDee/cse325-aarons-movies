6e5e1b668581a840c3b00a921f1d705b6930efa6
Aaron Delate <aaron@zero-one.io>
2026-03-14 17:55:14 +0200

    init: initial commit

--- obj/Debug/net10.0/MvcMovie.AssemblyInfo.cs
+++ obj/Debug/net10.0/MvcMovie.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+167ded726ed45e575678c46947eee2fcecf09f2c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7f53fab2c0849c80d9a8cc719f9a134eecc02d2f")]
 [assembly: System.Reflection.AssemblyProductAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
